c662b4944ac71da5194642211e47d51731695219
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-02-08 13:13:57 +0600

    Create route with active style and common layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import Head from "next/head";
+import Layout from "../components/Layout/Layout";
 import Main from "../components/Main/Main";
 
-export default function Home() {
+export default function Page() {
   return (
     <div className="bg-primary h-screen text-icon">
       <Head>
         <title>Carrot Stram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Main />
     </div>
   );
 }
+Page.getLayout = function getLayout(page: any) {
+  return (
+    <Layout>
+      {/* <NestedLayout>{page}</NestedLayout> */}
+      {page}
+    </Layout>
+  );
+};
