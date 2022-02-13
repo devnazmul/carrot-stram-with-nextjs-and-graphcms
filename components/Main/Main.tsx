35bfa8f3d67fec8cc36e0b3703ff05f2fbdef5f0
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-02-13 11:16:39 +0600

    revert previous change

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,8 +1,10 @@
 import moment from "moment";
-import { GetStaticProps } from "next";
-import React, { useEffect, useState } from "react";
-import { getVideosForHomePage } from "../../services/index";
+import React from "react";
 import VideoCard from "./VideoCard/VideoCard";
+
+interface Props {
+  videos: any;
+}
 
 interface Video {
   publishedAt: string;
@@ -30,17 +32,7 @@ interface Avatar {
   url: string;
 }
 
-export default function Main() {
-  const [videos, setVideos] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    getVideosForHomePage().then((data) => {
-      setVideos(data);
-      setIsLoading(false);
-    });
-  }, []);
-
+export default function Main({ videos }: Props) {
   return (
     <div className="bg-orange text-gray-600 body-font bg-transparent pl-24 lg:pl-72 mt-10">
       <div className="container px-5 py-24 mx-auto">
@@ -49,34 +41,22 @@ export default function Main() {
             </div> */}
 
         <div className="flex flex-wrap -m-3">
-          {isLoading ? (
-            <div className="w-96 flex items-center justify-center h-screen text-2xl">
-              Loading...
-            </div>
-          ) : (
-            videos.map((video: Video) => (
-              <>
-                <VideoCard
-                  key={video.id}
-                  videoUrl={video.id}
-                  thumbnailUrl={video.thumbnail.url}
-                  title={video.title}
-                  channelName={video.channel.channelName}
-                  channelAvater={video.channel.channelLogo.url}
-                  timeStamp={moment(video.publishedAt).fromNow()}
-                  views={video.views.length}
-                />
-              </>
-            ))
-          )}
+          {videos.map((video: Video) => (
+            <React.Fragment key={video.id}>
+              <VideoCard
+                videoUrl={video.id}
+                thumbnailUrl={video.thumbnail.url}
+                title={video.title}
+                channelName={video.channel.channelName}
+                channelAvater={video.channel.channelLogo.url}
+                timeStamp={moment(video.publishedAt).fromNow()}
+                views={video.views.length}
+              />
+            </React.Fragment>
+          ))}
           {/* <Finised></Finised> */}
         </div>
       </div>
     </div>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const videos = (await getVideosForHomePage()) || [];
-  return { props: { videos } };
-};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import Main from "../components/Main/Main";
+import { getVideosForHomePage } from "../services";
 
-export default function Page() {
+export const getStaticProps: GetStaticProps = async () => {
+  const videos = (await getVideosForHomePage()) || [];
+  return { props: { videos } };
+};
+
+interface Props {
+  videos: Array<object>;
+}
+
+export default function Page({ videos }: Props) {
   return (
     <div className="bg-primary h-screen text-icon">
       <Head>
         <title>Carrot Stram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Main />
+      <Main videos={videos} />
     </div>
   );
 }
