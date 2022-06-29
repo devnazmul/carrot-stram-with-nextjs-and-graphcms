15eb0dd3b9b3de917f1cbe20abc08c46889e28e3
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-06-29 14:17:09 +0600

    Create SignUp Page

--- pages/signUp/index.tsx
+++ pages/signUp/index.tsx
@@ -1,5 +1,4 @@
 import { Orbit } from "@uiball/loaders";
-import { gql, GraphQLClient } from "graphql-request";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -12,37 +11,6 @@ export default function index() {
   const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
   const router = useRouter();
-
-  const test = (data: any) => {
-    const GraphQLclient = new GraphQLClient(
-      `https://api-ap-south-1.graphcms.com/v2/ckyyobxtf05dl01zchugfh4jv/master`,
-      {
-        headers: {
-          authorizaton: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2NDQxNTQyNzIsImF1ZCI6WyJodHRwczovL2FwaS1hcC1zb3V0aC0xLmdyYXBoY21zLmNvbS92Mi9ja3l5b2J4dGYwNWRsMDF6Y2h1Z2ZoNGp2L21hc3RlciIsImh0dHBzOi8vbWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyIsInN1YiI6ImFmMDMwZGE2LTNhYzctNDM3MS05NjFmLTJkN2YxYjczYjFjNCIsImp0aSI6ImNremJiMDQyMzJkam4wMXl4aDJuMDdwcWwifQ.4UZABPgtuzDM7e7CeqZFjIlXw8_SFo3Kce1jRuxqoQZ29ED9NbzSiSOFLxNquqDYYTJbhVhRqctIZ1z_A1KJFJAo8LtsBFiMCiT--bQUaLwe3jGAY6HsM8eisxE_ZCiQeIdDgIvX2hwJynkXWNCENNuV0vPYgRDytYQQ_mtxTsnp-iBAdioa4gma_46dl3Kf70OAcTb3DSNCoVVUeDU5ouflq8p7sysR9yHRHKh1KGdFkYO-BQM7QOxes_hIQRqc2ltDwMo1A7bGzRVZEoPwSFas6f7TKbeIBGOgEeKV_sNP8Mx9VvtF64UulnLE1vaQGEShAtwQQ6WVdH9HB2hMG0lhOZJWDdwMblcZlf-w40cjFhvMezY0wMt2-M4TO7h3oaZv31J8K-RMcle6W6FjS_x2S7v7TbG5u37kD-95qRkieq5-udUD8kQUcXp9VfbQk8ZFfKW5W6VQZFCv5pQP0UsHwZrFwniI36ajy0Em7EotrYBdi_jopJa7ArrWbei_tQ7mgCljCDYo4CMfFEUFgkvE3vlkHAV5DD55459yoSPoEDWdVrGLCeGMLw8xKkUVRkqSXhoOL0mlN-5MhRJIFWd-JHsXV8C6Rira9ABCayyx9Q8Yl0dTqS72I8xPGFVU4n2H3_J5-eWvIsGlNYgkmQCd-wdkInDlm6ITJGhWt6U`,
-        },
-      }
-    );
-    const query = gql`
-      mutation MyMutation {
-        publishAuthor(
-          where: { id: "cl4yjqklp02dn0bphyk6r7y7b" }
-          to: PUBLISHED
-        ) {
-          email
-          avatar {
-            url
-          }
-          fullName
-          usename
-          about
-        }
-      }
-    `;
-
-    GraphQLclient.request(query).then((response) => {
-      console.log(response);
-    });
-  };
 
   const onSubmit = (data: any) => {
     // set loading true and scroll to top
