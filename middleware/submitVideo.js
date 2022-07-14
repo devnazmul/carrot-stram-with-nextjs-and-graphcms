9b65346aa2027745cc15b9e120e76d7f64075af2
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2022-07-14 17:11:48 +0600

    Update submitVideo.js

--- middleware/submitVideo.js
+++ middleware/submitVideo.js
@@ -4,67 +4,67 @@ import { updateAssetStage, updateVideoStage, uploadVideo } from "../services";
 export const submitVideo = async (obj) => {
     console.log(obj);
     const graphCMSAssetUploadEndpoint = process.env.NEXT_PUBLIC_GRAPHCMS_ASSET_UPLOAD_ENDPOINT;
-    // Upload Thumbnail
+    // Upload Video
     const form1 = new FormData();
     form1.append("fileUpload", obj.videoContent[0]);
     const result1 = await fetch(graphCMSAssetUploadEndpoint, {
         method: 'POST',
-        mode:'cors',
         headers: {
             Authorization: process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN,
         },
         body: form1,
-    }).then(response => {return response.json() })
-    // Upload Video
-    const form2 = new FormData();
-    form2.append("fileUpload", obj.thumbnail[0]);
-    const result2 = await fetch(graphCMSAssetUploadEndpoint, {
-        method: 'POST',
-        headers: {
-            Authorization: process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN,
-        },
-        body: form2,
-    }).then(response => {return response.json() })
-console.log('videoContent',result1);
-console.log('thumbnail',result2);
-    // Publish Thumbnail
-    updateAssetStage(result2.id).then((thumId) => {
-        if (thumId !== undefined) {
-            // Publish Video
-            updateAssetStage(result1.id).then((vidId) => {
-                if (vidId !== undefined) {
-                    // Upload Video
-                    const { title, description, slug } = obj;
-                    uploadVideo(title, description, thumId, vidId, slug).then((videoId) => {
-                        if (videoId !== undefined) {
-                            // Publish Video
-                            return updateVideoStage(videoId).then((publishedVideoSlug) => {
-                                return publishedVideoSlug;
-                            })
-                        } else {
-                            return {
-                                error: {
-                                    message: 'Uploading failed!'
-                                }
-                            }
-                        }
+    }).then(response => {return response.json() });
+    console.log(result1);
+// Upload Thumbnail
+//     const form2 = new FormData();
+//     form2.append("fileUpload", obj.thumbnail[0]);
+//     const result2 = await fetch(graphCMSAssetUploadEndpoint, {
+//         method: 'POST',
+//         headers: {
+//             Authorization: process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN,
+//         },
+//         body: form2,
+//     }).then(response => {return response.json() })
+// console.log('videoContent',result1);
+// console.log('thumbnail',result2);
+//     // Publish Thumbnail
+//     updateAssetStage(result2.id).then((thumId) => {
+//         if (thumId !== undefined) {
+//             // Publish Video
+//             updateAssetStage(result1.id).then((vidId) => {
+//                 if (vidId !== undefined) {
+//                     // Upload Video
+//                     const { title, description, slug } = obj;
+//                     uploadVideo(title, description, thumId, vidId, slug).then((videoId) => {
+//                         if (videoId !== undefined) {
+//                             // Publish Video
+//                             return updateVideoStage(videoId).then((publishedVideoSlug) => {
+//                                 return publishedVideoSlug;
+//                             })
+//                         } else {
+//                             return {
+//                                 error: {
+//                                     message: 'Uploading failed!'
+//                                 }
+//                             }
+//                         }
 
-                    })
-                } else {
-                    return {
-                        error: {
-                            message: 'Uploading failed!'
-                        }
-                    }
-                }
-            }
-            )
-        } else {
-            return {
-                error: {
-                    message: 'Uploading failed!'
-                }
-            }
-        }
-    })
+//                     })
+//                 } else {
+//                     return {
+//                         error: {
+//                             message: 'Uploading failed!'
+//                         }
+//                     }
+//                 }
+//             }
+//             )
+//         } else {
+//             return {
+//                 error: {
+//                     message: 'Uploading failed!'
+//                 }
+//             }
+//         }
+//     })
 }
